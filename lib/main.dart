047f4656a6e398df4cf678f780f53c205f9285a2
atsushiIMG <atusibba1014@yahoo.co.jp>
2023-07-15 18:04:48 +0900

    textThemeを追加

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,81 @@ class Home extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: SafeArea(
-        child: Text("hello"),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "displayLarge",
+                style: Theme.of(context).textTheme.displayLarge!.merge(
+                      TextStyle(
+                        color: Colors.amber,
+                      ),
+                    ),
+              ),
+              Text(
+                "displayMedium",
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              Text(
+                "displaySmall",
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              Text(
+                "headlineLarge",
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              Text(
+                "headlineMedium",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                "headlineSmall",
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              Text(
+                "titleLarge",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                "titleMedium",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Text(
+                "titleSmall",
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              Text(
+                "bodyLarge",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              Text(
+                "bodyMedium",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              Text(
+                "bodySmall",
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              Text(
+                "labelLarge",
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+              Text(
+                "labelMedium",
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+              Text(
+                "labelSmall",
+                style: Theme.of(context).textTheme.labelSmall!.merge(
+                      TextStyle(
+                        color: Colors.amber,
+                      ),
+                    ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
